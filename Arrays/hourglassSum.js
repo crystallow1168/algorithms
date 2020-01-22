953e19c3d5fd124494399fa911cd40a78114e343
Crystal.L <crystal.l0815@gmail.com>
2020-01-22 14:59:24 -0800

    Added hourglassSum solution

---
+++ Arrays/hourglassSum.js
@@ -0,0 +1,40 @@
+// https://www.hackerrank.com/challenges/2d-array/problem
+
+const input = [
+  [1, 1, 1, 0, 0, 0],
+  [0, 1, 0, 0, 0, 0],
+  [1, 1, 1, 0, 0, 0],
+  [0, 0, 2, 4, 4, 0],
+  [0, 0, 0, 2, 0, 0],
+  [0, 0, 1, 2, 4, 0]
+]
+
+// output = 19
+
+const hourglassSum = (arr) => {
+  let highestSum = -Infinity;
+  
+    for (let i = 0; i < arr.length - 2; i ++) {
+      const curArr = arr[i];
+  
+      for (let j = 0; j < curArr.length - 2; j ++) {
+        const sum = sumOfOneHourGlass(arr, i, j);
+  
+        if (sum >  highestSum) {
+          highestSum = sum;
+        }
+      }
+    }
+  return highestSum;
+  
+}
+  
+const sumOfOneHourGlass = (arr, row, column) => {
+  let sumOfRow1 = arr[row][column] + arr[row][column + 1] + arr[row][column + 2];
+  let sumOfRow2 = arr[row + 1][column + 1];
+  let sumOfRow3 = arr[row + 2][column] + arr[row + 2][column + 1] + arr[row + 2][column + 2];
+
+  return sumOfRow1 + sumOfRow2 + sumOfRow3;
+}
+  
+console.log(hourglassSum(input))
